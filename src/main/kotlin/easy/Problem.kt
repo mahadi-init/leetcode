d24c7bf3b107092d483229a1fd07f236a4386007
mahadi-init <mahadi.init@proton.me>
2023-10-12 10:37:22 +0600

    remove submission urls

--- src/main/kotlin/easy/Problem.kt
+++ src/main/kotlin/easy/Problem.kt
@@ -1,44 +1,44 @@
 package easy
 
 interface Problem {
-    fun twoSum(nums: IntArray, target: Int): IntArray //https://leetcode.com/problems/two-sum/submissions/1071537101/
+    fun twoSum(nums: IntArray, target: Int): IntArray
 
-    fun isPalindrome(x: Int): Boolean //https://leetcode.com/problems/palindrome-number/submissions/1071543158/
+    fun isPalindrome(x: Int): Boolean
 
-    fun maxProfit(prices: IntArray): Int //https://leetcode.com/problems/best-time-to-buy-and-sell-stock/submissions/1071546343/
+    fun maxProfit(prices: IntArray): Int
 
-    fun containsDuplicate(nums: IntArray): Boolean  //https://leetcode.com/problems/contains-duplicate/submissions/1071548613/
+    fun containsDuplicate(nums: IntArray): Boolean
 
-    fun isAnagram(s: String, t: String): Boolean  //https://leetcode.com/problems/valid-anagram/submissions/1071555140/
+    fun isAnagram(s: String, t: String): Boolean
 
-    fun fib(n: Int): Int  //https://leetcode.com/problems/fibonacci-number/submissions/1071557763/
+    fun fib(n: Int): Int
 
-    fun reverseWords(s: String): String  //https://leetcode.com/problems/reverse-words-in-a-string-iii/submissions/1071577552/
+    fun reverseWords(s: String): String
 
-    fun search(nums: IntArray, target: Int): Int  //https://leetcode.com/problems/binary-search/submissions/1071582189/
+    fun search(nums: IntArray, target: Int): Int
 
-    fun reverseString(s: CharArray)  //https://leetcode.com/problems/reverse-string/submissions/1071587097/
+    fun reverseString(s: CharArray)
 
-    fun minDeletionSize(strs: Array<String>): Int  //https://leetcode.com/problems/delete-columns-to-make-sorted/submissions/1071588507/
+    fun minDeletionSize(strs: Array<String>): Int
 
-    fun sortedSquares(nums: IntArray): IntArray //https://leetcode.com/problems/squares-of-a-sorted-array/submissions/1072361229/
+    fun sortedSquares(nums: IntArray): IntArray
 
-    fun buildArray(nums: IntArray): IntArray  //https://leetcode.com/problems/build-array-from-permutation/submissions/1072363223/
+    fun buildArray(nums: IntArray): IntArray
 
-    fun getConcatenation(nums: IntArray): IntArray  //https://leetcode.com/problems/concatenation-of-array/submissions/1072542362/
+    fun getConcatenation(nums: IntArray): IntArray
 
-    fun finalValueAfterOperations(operations: Array<String>): Int // https://leetcode.com/problems/final-value-of-variable-after-performing-operations/submissions/1072544722/
+    fun finalValueAfterOperations(operations: Array<String>): Int
 
     fun shuffle(
         nums: IntArray,
         n: Int
-    ): IntArray // https://leetcode.com/problems/shuffle-the-array/submissions/1073142188/
+    ): IntArray
 
-    fun runningSum(nums: IntArray): IntArray  //https://leetcode.com/problems/running-sum-of-1d-array/submissions/1073146560/
+    fun runningSum(nums: IntArray): IntArray
 
-    fun numIdenticalPairs(nums: IntArray): Int //https://leetcode.com/problems/number-of-good-pairs/submissions/1073149341/
+    fun numIdenticalPairs(nums: IntArray): Int
 
-    fun maximumWealth(accounts: Array<IntArray>): Int //https://leetcode.com/problems/richest-customer-wealth/submissions/1073152733/
+    fun maximumWealth(accounts: Array<IntArray>): Int
 
-    fun mostWordsFound(sentences: Array<String>): Int  //https://leetcode.com/problems/maximum-number-of-words-found-in-sentences/submissions/1073154687/
+    fun mostWordsFound(sentences: Array<String>): Int
 }
